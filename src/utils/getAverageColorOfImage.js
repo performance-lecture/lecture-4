d9a07ecb07f0dd169206337c16fe40c32c568ab3
hackurity01 <hackurity01@gmail.com>
2021-06-13 00:30:58 +0900

    4-7 병목 함수에 memoization 적용

--- src/utils/getAverageColorOfImage.js
+++ src/utils/getAverageColorOfImage.js
@@ -1,4 +1,10 @@
+const cache = {};
+
 export function getAverageColorOfImage(imgElement) {
+  if (cache.hasOwnProperty(imgElement.src)) {
+    return cache[imgElement.src];
+  }
+
   const canvas = document.createElement('canvas');
   const context = canvas.getContext && canvas.getContext('2d');
   const averageColor = {
@@ -31,6 +37,8 @@ export function getAverageColorOfImage(imgElement) {
   averageColor.r = ~~(averageColor.r / count); // ~~ => convert to int
   averageColor.g = ~~(averageColor.g / count);
   averageColor.b = ~~(averageColor.b / count);
+
+  cache[imgElement.src] = averageColor;
 
   return averageColor;
 }
