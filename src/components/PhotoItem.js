53e3c4b9236d9fd1dd343193c2a587f000ea327f
hackurity01 <hackurity01@gmail.com>
2022-08-28 14:32:30 +0900

    4-4 이미지 지연 로딩

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { showModal } from '../redux/imageModal';
+import LazyLoad from 'react-lazyload';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
@@ -12,7 +13,9 @@ function PhotoItem({ photo: { urls, alt } }) {
 
   return (
     <ImageWrap>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <LazyLoad offset={1000}>
+        <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      </LazyLoad>
     </ImageWrap>
   );
 }
