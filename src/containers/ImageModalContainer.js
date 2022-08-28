2dd118d340c9c8509f152c99aceb9f8186f14ec9
hackurity01 <hackurity01@gmail.com>
2022-08-28 14:32:54 +0900

    4-5 리덕스 렌더링 최적화

--- src/containers/ImageModalContainer.js
+++ src/containers/ImageModalContainer.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const { modalVisible, bgColor, src, alt } = useSelector(state => ({
-    modalVisible: state.imageModal.modalVisible,
-    bgColor: state.imageModal.bgColor,
-    src: state.imageModal.src,
-    alt: state.imageModal.alt,
-  }));
-
-  return (
-    <ImageModal
-      modalVisible={modalVisible}
-      bgColor={bgColor}
-      src={src}
-      alt={alt}
-    />
+  const { modalVisible, bgColor, src, alt } = useSelector(
+    state => ({
+      modalVisible: state.imageModal.modalVisible,
+      bgColor: state.imageModal.bgColor,
+      src: state.imageModal.src,
+      alt: state.imageModal.alt,
+    }),
+    shallowEqual
   );
+
+  return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
 
 export default ImageModalContainer;
